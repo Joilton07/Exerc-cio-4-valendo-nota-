a66bbe47d64b506a2447c0fbb2a937dc3f7b6208
fco-felix <felix@aluno.fapce.edu.br>
2020-04-20 23:40:50 -0300

    Layout changed; hint in TextBox; ENTER as TAB.

--- Exercicio4/Form1.cs
+++ Exercicio4/Form1.cs
@@ -10,21 +10,69 @@ using System.Windows.Forms;
 
 namespace Exercicio4
 {
-    public partial class Form1 : Form
+    public partial class Form_Usuario : Form
     {
-        public Form1()
+        public Form_Usuario()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button_Acessar_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Verificar validade do usuário e senha");
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
+        private void button_Sair_Click(object sender, EventArgs e) => Close();
 
+        private void textBox_Usuario_Enter(object sender, EventArgs e)
+        {
+            if (textBox_Usuario.Text == "Informe seu nome de usuário")
+            {
+                textBox_Usuario.Text = "";
+                textBox_Usuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox_Usuario_Leave(object sender, EventArgs e)
+        {
+            if (textBox_Usuario.Text == "")
+            {
+                textBox_Usuario.Text = "Informe seu nome de usuário";
+                textBox_Usuario.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBox_Senha_Leave(object sender, EventArgs e)
+        {
+            if (textBox_Senha.Text == "")
+            {
+                textBox_Senha.Text = "Informe sua senha";
+                textBox_Senha.ForeColor = Color.Gray;
+                textBox_Senha.PasswordChar = (char)0;
+            }
+        }
+
+        private void textBox_Senha_Enter(object sender, EventArgs e)
+        {
+            if (textBox_Senha.Text == "Informe sua senha")
+            {
+                textBox_Senha.Text = "";
+                textBox_Senha.ForeColor = Color.Black;
+                textBox_Senha.PasswordChar = '*';
+            }
+        }
+
+        private void Form_Usuario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                SelectNextControl(ActiveControl, true, true, true, true);
+            }
+        }
+
+        private void Form_Usuario_Load(object sender, EventArgs e)
+        {
         }
     }
 }
